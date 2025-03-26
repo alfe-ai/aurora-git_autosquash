1996709c2485af60399bfbabed84230b16419abd
whimsy <whimsy@sylph.box>
2025-03-25 23:48:36 -0500

    • Updated cloneAndSquash.js to force-checkout the "squash_branch" instead of failing when the branch already exists.

--- cloneAndSquash.js
+++ cloneAndSquash.js
@@ -8,7 +8,7 @@ module.exports = async function cloneAndSquash(source, destination, skipClone) {
             await git.clone(source, destination);
         }
         await git.cwd(destination);
-        await git.checkout(['-b', 'squash_branch']);
+        await git.checkout(['-B', 'squash_branch']);
         await git.reset(['--hard', 'HEAD~1']);
         await git.add('./*');
         await git.commit('Squashed commit');
