e6ff2571d27c98d1d3e4f259441ca7abf599bb96
whimsy <whimsy@sylph.box>
2025-03-25 23:49:55 -0500

    - Modified cloneAndSquash.js to skip resetting to HEAD~1 if there is only one commit in the repository.

--- cloneAndSquash.js
+++ cloneAndSquash.js
@@ -9,7 +9,14 @@ module.exports = async function cloneAndSquash(source, destination, skipClone) {
         }
         await git.cwd(destination);
         await git.checkout(['-B', 'squash_branch']);
-        await git.reset(['--hard', 'HEAD~1']);
+
+        const logs = await git.log();
+        if (logs.total > 1) {
+            await git.reset(['--hard', 'HEAD~1']);
+        } else {
+            console.log('No previous commit. Skipping reset.');
+        }
+
         await git.add('./*');
         await git.commit('Squashed commit');
     } catch (err) {
